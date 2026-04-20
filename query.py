7cde42fa21f3413879ba9792f066122b148a678e
haseeb-heaven <11544739+haseeb-heaven@users.noreply.github.com>
2026-04-20 19:14:36 +0000

    chore: remove junk scratchpad files and update .gitignore

--- query.py
+++
@@ -1,6 +0,0 @@
-import subprocess, json
-try:
-    res = subprocess.check_output(['D:/Code/gworkspace-agent/gws.exe', 'drive', 'files', 'list', '--params', '{"q": "name=\'temp_inventory_store\' and trashed=false", "fields": "files(id, name, mimeType, size)"}']).decode('utf-8')
-    print(json.dumps(json.loads(res), indent=2))
-except Exception as e:
-    print(e)
